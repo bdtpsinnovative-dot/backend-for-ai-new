dc1adea7477bcdf58c3903d80345df498821d6e0
twanty979-hue <twanty979@gmail.com>
2026-03-02 19:50:51 +0700

    twstttttt

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+// 1. เปลี่ยนชื่อฟังก์ชันจาก middleware เป็น proxy
+export function proxy(request: NextRequest) {
   // สร้าง response
   const response = NextResponse.next()
 
   // ✅ ตั้งค่า CORS เพื่อให้ Flutter Web (localhost) หรือ Domain อื่นเรียกใช้ได้
-  response.headers.set('Access-Control-Allow-Origin', '*') // หรือใส่เฉพาะ URL ของ Flutter Web
+  response.headers.set('Access-Control-Allow-Origin', '*') 
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
 
@@ -18,7 +19,7 @@ export function middleware(request: NextRequest) {
   return response
 }
 
-// กำหนดให้ Middleware ทำงานเฉพาะกับ API เท่านั้น
+// กำหนดให้ Proxy ทำงานเฉพาะกับ API เท่านั้น
 export const config = {
   matcher: '/api/:path*',
 }
